98641395f074f2f482f724edce70c804b47bc7e7
cpetersen8 <cpetersen8@csc.com>
2018-09-08 14:55:18 +1000

    Google Maps integration

--- govhack/src/components/Map.vue
+++ govhack/src/components/Map.vue
@@ -3,35 +3,42 @@
     <div class="flex-container">
       <div class="options">
         <p class="title">TEAM DXC!!!!!!!!! </p>
+          <div id="app">
+            <google-map />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Map',
-  data () {
-    return {
+  import GoogleMap from "../components/GoogleMap";
+    export default {
+          name: 'Map',
+            components: {
+            GoogleMap
+        },
+        data () {
+        return {
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.home {
-  padding-bottom: 2em;
-  flex-grow: 1;
-  overflow-y: auto;
-}
+  .home {
+    padding-bottom: 2em;
+    flex-grow: 1;
+    overflow-y: auto;
+  }
 
-.button {
-  border: 1px solid black;
-  padding: 1em;
-}
+  .button {
+    border: 1px solid black;
+    padding: 1em;
+  }
 
-.options {
-  padding: 3em;
-}
+  .options {
+    padding: 3em;
+  }
 </style>
