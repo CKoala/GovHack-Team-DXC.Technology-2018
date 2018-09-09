6decd5188afd6daa4c0238d8a9a6a5995cc65ef4
Niloy Chowdhury <niloy.chowdhury@outlook.com>
2018-09-09 16:25:15 +1000

    Upload data files used

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -5,15 +5,7 @@ import filterReducer from "../reducers/filterReducer";
 const allStoreEnhancers = getEnchancers();
 
 function getEnchancers() {
-  if (process.env.REACT_APP_ENV === "production") {
-    return compose(applyMiddleware(thunk));
-  } else {
-    return compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
-  }
+  return compose(applyMiddleware(thunk));
 }
 
 export default function create(initialState) {
